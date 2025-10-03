1ca7a7e3d465428ec7d5a7a27358e2b0a3fc6a39
iagoliziero <iagolizierox14@gmail.com>
2025-10-03 11:55:05 -0300

    fix: adjusting error in dto data

--- src/main/java/br/com/fiap/dao/MedicineDAO.java
+++ src/main/java/br/com/fiap/dao/MedicineDAO.java
@@ -9,7 +9,7 @@ public class MedicineDAO {
     public ArrayList<MedicineTO> findAll() {
 
         ArrayList<MedicineTO> medicines = new ArrayList<>();
-        MedicineTO medicine = new MedicineTO(1L, "Loratadine", 7.93, LocalDate.parse("2023-10-10"), LocalDate.parse("20265-10-10"));
+        MedicineTO medicine = new MedicineTO(1L, "Loratadine", 7.93, LocalDate.parse("2023-10-10"), LocalDate.parse("2026-10-10"));
         medicines.add(medicine);
 
         medicine = new MedicineTO(2L, "Amoxiline", 26.50, LocalDate.now(), LocalDate.now().plusYears(2));
